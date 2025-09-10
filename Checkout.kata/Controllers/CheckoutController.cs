69bc9931f26851824db366b17f5b27e913c0ea90
ArslanDotNetDev <arslan.netdev@gmail.com>
2025-09-10 07:07:49 +0500

    Commit for application structure

---
+++ Checkout.kata/Controllers/CheckoutController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Checkout.kata.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CheckoutController : ControllerBase
+    {
+        public CheckoutController()
+        {
+        }
+
+        
+    }
+}
